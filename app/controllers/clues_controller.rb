770140f273656218478f63c1850098d6df91a043
ShahidArshad2901 <shahid.arshad@devsinc.com>
2023-02-14 09:05:17 -0800

    update controller

--- app/controllers/clues_controller.rb
+++ app/controllers/clues_controller.rb
@@ -1,4 +1,5 @@
 class CluesController < ApplicationController
+  before_action :set_current_question, only: %i[quiz question_response]
   def index
     # This service curently using dummy Endpoint
     Clue.create(file_content: QuestionService.fetch_data)
@@ -7,22 +8,16 @@ class CluesController < ApplicationController
 
   def quiz
     @conclusion = session[:conclusion] || nil
-    @question = set_current_question
+    @question = @current_question
   end
 
   def question_response
-    result = session[:result]
-    current_question = set_current_question
     response = clue_params[:response].downcase
   
-    context = current_question.dig("answers", response, "context")
-    result << context
-    next_question_id = current_question.dig("answers", response, "question_id")
-  
-    session[:result] = result
-    session[:current_question_id] = next_question_id
+    session[:result] << @current_question.dig("answers", response, "context")
+    session[:current_question_id] = @current_question.dig("answers", response, "question_id")
 
-    session[:conclusion] = determine_conclusion(result)
+    session[:conclusion] = determine_conclusion
     redirect_to clues_quiz_path
   end
 
@@ -30,18 +25,19 @@ class CluesController < ApplicationController
 
   def reset_session
     session.clear
-    session[:questions] = Clue.last.file_content["questions"]
-    session[:conclusions] = Clue.last.file_content["conclusions"]
+    clue = Clue.last.file_content
+    session[:questions] = clue["questions"]
+    session[:conclusions] = clue["conclusions"]
     session[:result] = []
     session[:current_question_id] = session[:questions].first["id"]
   end
 
   def set_current_question
-    session[:questions].find {|question| question["id"] == session[:current_question_id]}
+    @current_question = session[:questions].find {|question| question["id"] == session[:current_question_id]}
   end
 
-  def determine_conclusion(result)
-    conclusion =  session[:conclusions].find{|con| con["context"] == result}
+  def determine_conclusion
+    conclusion = session[:conclusions].find{|con| con["context"] == session['result']}
     conclusion&.dig("conclusion")
   end
 
